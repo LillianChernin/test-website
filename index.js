6ab483d0f2e60b232300f16a04e112bbd6040a44
LillianChernin <l.smith1654@gmail.com>
2017-11-26 21:49:57 -0600

    replace code with heroku test

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var expressLib = require('express');
-var webServer = expressLib();
-webServer.set('port', process.env.PORT);
-webServer.use(express.static(__dirname + 'public'));
-webServer.get('/', function (req, res) {
-    res.send('hello world')
-})
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
